4a711b87ed447746658ce92ff000debbdeb74a91
Denis Kvasnitsyn <kvasnicyn@gmail.com>
2023-06-15 19:40:49 +0500

    edit App.java with Hibernate magic edit POM.xml with java 11 version edit hibernate.properties with typo

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,35 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //подключим конфигурацию из файла hibernate.properties (не явно)
+        //укажем сущность Person
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        //создаем фабрику сессий
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        //получаем сессию
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+
+            //для примера получим сущность из таблицы в дб
+            Person person = session.get(Person.class, 1); //получаем Person с id == 1
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            //закроем сессию
+            session.getTransaction().commit();
+        } finally {
+            //зкароем фабрику
+            sessionFactory.close();
+        }
     }
 }
